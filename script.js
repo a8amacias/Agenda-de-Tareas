fc7057b4c6b535c5e76bda96ae3777a94cc05723
a8amacias <abrilochoamacias@gmail.com>
2026-09-24 10:30:34 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,59 +1,65 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyD_ZFW_XtDmRidIt4Q_GEhT2OsMUR_UtSA",
+  authDomain: "agenda-de-tareas-f6d01.firebaseapp.com",
+  projectId: "agenda-de-tareas-f6d01",
+  storageBucket: "agenda-de-tareas-f6d01.firebasestorage.app",
+  messagingSenderId: "795424230539",
+  appId: "1:795424230539:web:e1ec344db3a9697632d88e"
+
+};
+firebase.initializeApp(firebaseConfig);
+const db = firebase.firestore();
+
 // Referencias a los elementos
 const tareaInput = document.getElementById("tareaInput");
 const agregarBtn = document.getElementById("agregarBtn");
 const borrarTodoBtn = document.getElementById("borrarTodoBtn");
 const listaTareas = document.getElementById("listaTareas");
 
-//Guardar tareas en localStorage
-function guardarTareas() {
-  const tareas = [];
-  listaTareas.querySelectorAll("li").forEach(li => {
-    const texto = li.firstChild.textContent.trim(); // solo el texto de la tarea
-    tareas.push(texto);
-  });
-  localStorage.setItem("misTareas", JSON.stringify(tareas));
-}
-
-//Cargar tareas al iniciar
-function cargarTareas() {
-  const tareasGuardadas = JSON.parse(localStorage.getItem("misTareas")) || [];
-  tareasGuardadas.forEach(tarea => {
-    crearTarea(tarea);
-  });
-}
-
 //Crear tarea con botón eliminar
-function crearTarea(texto) {
+function crearTarea(texto, id) {
   const li = document.createElement("li");
   li.textContent = texto;
 
   const eliminarBtn = document.createElement("button");
   eliminarBtn.textContent = "Eliminar";
   eliminarBtn.classList.add("eliminarBtn");
-  eliminarBtn.addEventListener("click", () => {
-    listaTareas.removeChild(li);
-    guardarTareas(); // actualizar almacenamiento
+  eliminarBtn.addEventListener("click", async () => {
+    if (id) {
+        await db.collection("tareas").doc(id).delete();
+    }
   });
 
   li.appendChild(eliminarBtn);
   listaTareas.appendChild(li);
 }
 
-//Agregar tarea
-agregarBtn.addEventListener("click", () => {
+//Agregar tarea (guarda los datos en la nube)
+agregarBtn.addEventListener("click", async() => {
   const texto = tareaInput.value.trim();
   if (texto !== "") {
-    crearTarea(texto);
-    guardarTareas();
-    tareaInput.value = "";
+     await db.collection("tareas").add({
+            titulo: texto,
+            completada: false,
+            creadaEn: firebase.firestore.FieldValue.serverTimestamp()
+    });
+  tareaInput.value = "";
   }
 });
 
-//Borrar toda la lista
-borrarTodoBtn.addEventListener("click", () => {
-  listaTareas.innerHTML = "";
-  localStorage.removeItem("misTareas"); // limpiar almacenamiento
-});
+//Borrar toda la lista (borra todos los documentos)
+borrarTodoBtn.addEventListener("click", async() => {
+ const snapshot = await db.collection("tareas").get();
+    snapshot.forEach(async (doc) => {
+        await db.collection("tareas").doc(doc.id).delete();
+       });
+   });
 
-//Al cargar la página
-window.onload = cargarTareas;
+  // Leer tareas en tiempo real desde Firestore
+db.collection("tareas").onSnapshot((snapshot) => {
+    listaTareas.innerHTML = "";
+    snapshot.forEach((doc) => {
+        const tarea = doc.data();
+        crearTarea(tarea.titulo, doc.id);
+    });
+});
